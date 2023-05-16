174e888834daad78d8b55c6f289c47585a086ee0
Rokyl <Shidlovskiy-02@mail.ru>
2023-05-16 17:42:20 +0300

    Added profiles creation

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,22 +1,33 @@
 class ProfilesController < ApplicationController
   before_action :get_profile if current_user
   def show
-    @profile
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to @profile
+    end
   end
 
   def edit
   end
 
   def update
+    if @profile.update(profile_params)
+      redirect_to @profile
+    else
+      redirect_to edit_profile_path
+    end
   end
 
   private
 
   def profile_params
-
+    params.require(:profile).permit!(:user_id, :weigh, :height, :age, :sex, :activity)
   end
 
   def get_profile
-    Profile.where(user_id: current_user)
+    @profile = Profile.where(user_id: current_user.id)
   end
 end
--- app/services/calculate_standards.rb
+++ app/services/calculate_standards.rb
@@ -1,17 +1,20 @@
 require 'dry/monads'
 class CalculateStandards
+  MODIFIERS = {1 => 1.2, 2 => 1.375, 3 => 1.725, 4 => 1.9}.freeze
   include Dry::Monads[:result]
 
-  def initialize(profile_id)
-    @profile = profile_find(profile_id)
+  def initialize(id)
+    @profile = profile_find(id)
+    return nil unless @profile
     @age = @profile.age
     @sex = @profile.sex
     @weight = @profile.weigh
     @height = @profile.height
-    @activity_multiplier = @profile.activity
+    @activity_multiplier = MODIFIERS[@profile.activity]
   end
 
   def call
+    return Failure(:profile_not_found) unless @profile
     if @sex==1
       @profile.update(BMR: mans_metabolic_rate.round)
       Success(@profile)
@@ -24,13 +27,7 @@ class CalculateStandards
   private
 
   def profile_find(profile_id)
-    profile = Profile.find(profile_id)
-
-    if profile
-      Success(profile)
-    else
-      Failure(:profile_not_found)
-    end
+    Profile.find_by(id: profile_id)
   end
 
   def mans_metabolic_rate
