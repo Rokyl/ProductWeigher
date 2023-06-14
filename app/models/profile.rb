963dc41623dc719bbe29264483a569e86174cb31
Rokyl <Shidlovskiy-02@mail.ru>
2023-06-14 16:16:10 +0300

    Updated profile logic

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,12 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many :ingestions
+  attr_reader :BMR
+  before_save :calculate_bmr
+
+  private
+
+  def calculate_bmr
+    CalculateStandards.call(self.id)
+  end
 end
