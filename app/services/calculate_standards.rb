d76ab39a8d6e296976e31236583183f2bb7a30c8
AndreiLazarus <andrei.shydlouski@lazarussoftware.com>
2023-07-20 15:41:08 +0300

    Fixed

--- app/services/calculate_standards.rb
+++ app/services/calculate_standards.rb
@@ -1,28 +1,37 @@
 class CalculateStandards < ApplicationService
-  MANS_METABOLIC_MULTI = { common: 88.36, weight: 13.4, height: 4.8, age: 5.7 }
-  WOMANS_METABOLIC_MULTI = { common: 447.6, weight: 9.2, height: 3.1, age: 4.3 }
+  MALE_METABOLIC_MULTI = { common: 88.36, weight: 13.4, height: 4.8, age: 5.7 }.freeze
+  FEMALE_METABOLIC_MULTI = { common: 447.6, weight: 9.2, height: 3.1, age: 4.3 }.freeze
   MODIFIERS = { 1 => 1.2, 2 => 1.375, 3 => 1.725, 4 => 1.9 }.freeze
 
   attr_reader :age, :sex, :weight, :height, :activity
+  attr_accessor :coefficients
 
   def initialize(profile)
     @profile = profile
-    age = @profile.age
-    sex = @profile.sex
-    weight = @profile.weigh
-    height = @profile.height
-    activity = MODIFIERS[@profile.activity]
+    @age = @profile.age
+    @sex = @profile.sex
+    @weight = @profile.weigh
+    @height = @profile.height
+    @activity = MODIFIERS[@profile.activity]
   end
 
   def call
-    rates = sex == 1 ? MANS_METABOLIC_MULTI : WOMANS_METABOLIC_MULTI
-    @profile.update(BMR: calculate(rates).round)
+    coefficients = sex == 1 ? MALE_METABOLIC_MULTI : FEMALE_METABOLIC_MULTI
+    @profile.update(BMR: calculate.round)
     Success(@profile)
   end
 
   private
-  def calculate(rates)
-    (rates[:common] + (rates[:weight] * weight) + (rates[:height] * height) - (rates[:age] * age)) * activity
+  def male?
+    sex == 1
+  end
+  
+  def female?
+    sex == 2
+  end
+  def calculate
+    coefficients = male? ? MALE_METABOLIC_MULTI : FEMALE_METABOLIC_MULTI
+    (coefficients[:common] + (coefficients[:weight] * weight) + (coefficients[:height] * height) - (coefficients[:age] * age)) * activity
   end
 
 end
